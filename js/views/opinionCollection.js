3d6b5af2b1d4cd69d1c4b86f62f688c45af31523
Callum Song <callumsong@mwam.home>
2016-06-01 20:24:48 +0100

    new opinions can be added

--- js/views/opinionCollection.js
+++ js/views/opinionCollection.js
@@ -6,6 +6,7 @@ app.OpinionCollection = Backbone.View.extend({
   initialize: function(initialOpinions) {
     this.collection = new app.Opinions(initialOpinions);
     this.render();
+    this.listenTo(this.collection, "add", this.renderOpinion);
   },
   
   render: function() {
@@ -19,5 +20,23 @@ app.OpinionCollection = Backbone.View.extend({
       model: item
     });
     this.$el.append(opinionView.render().el);
+  },
+  
+  events: {
+    "click #addNewOpinion":"addOpinion"
+  },
+  
+  addOpinion: function(e) {
+    e.preventDefault();
+    
+    var formData = {};
+    
+    $("#addOpinion div").children("input, textarea").each(function(i, el) {
+      if ($(el).val() != "") {
+        formData[el.id] = $(el).val();
+      }
+    });
+    
+    this.collection.add(new app.Opinion(formData));
   }
 });
